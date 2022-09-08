a8b3b739192cfc5893531667f2f8d30d097bbd53
Alicia Moreno <lisireyes93@yahoo.com>
2022-09-08 18:56:38 -0500

    first commit

---
+++ DataSeeding/0_1_0_Users.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO users (`id`,`username`) VALUES
+(1,'lbernal'),
+(2,'amoreno'),
+(3,'gbernal');
---
+++ Migrations/20220908233710_0_1_0_Users_Data_Seeds.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class _0_1_0_Users_Data_Seeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assemblyFullPath = System.Reflection.Assembly.GetAssembly(typeof(DataContext)).Location;
+            var assemblyPath = System.IO.Path.GetDirectoryName(assemblyFullPath);
+
+            var sqlFile = System.IO.Path.Combine(assemblyPath, "DataSeeding\\0_1_0_Users.sql");
+            migrationBuilder.Sql(System.IO.File.ReadAllText(sqlFile));
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
